c3d91d04504198eff8c6e0cf669824d98e4f07a8
danilooliver101 <danilo.oliveira142@gmail.com>
2026-03-10 22:43:01 -0300

    refactor(cleanup): remove unused utility classes

--- src/main/kotlin/com/heitor/finance/shared/constants/AppConstants.kt
+++
@@ -1,7 +0,0 @@
-package com.heitor.finance.shared.constants
-
-object AppConstants {
-    const val API_VERSION = "v1"
-    const val API_KEY_HEADER = "api-key"
-    const val DATE_PATTERN = "yyyy-MM-dd"
-}
